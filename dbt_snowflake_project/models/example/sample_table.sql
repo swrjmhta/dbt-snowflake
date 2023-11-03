4dacf57acc3495c8d3590fb6f28baad93f80af17
swrjmhta <swrjmhta@gmail.com>
2023-11-03 11:48:47 +0530

    second commit

---
+++ dbt_snowflake_project/models/example/sample_table.sql
@@ -0,0 +1,28 @@
+{{
+  config(
+    materialized = "table"
+  )
+}}
+SELECT 'January' Mon
+
+UNION ALL    SELECT 'February' Mon
+
+UNION ALL    SELECT 'March' Mon
+
+UNION ALL    SELECT 'April' Mon
+
+UNION ALL    SELECT 'May' Mon
+
+UNION ALL    SELECT 'June' Mon
+
+UNION ALL    SELECT 'July' Mon
+
+UNION ALL    SELECT 'August' Mon
+
+UNION ALL    SELECT 'September' Mon
+
+UNION ALL    SELECT 'October' Mon
+
+UNION ALL    SELECT 'November' mon
+
+UNION ALL    SELECT 'December' mon
